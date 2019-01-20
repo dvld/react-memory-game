0657594819557d908aaa63f7cfb6fc82cb66445f
dvld <nikoroberts620@gmail.com>
2019-01-19 23:39:49 -0600

    Updates

---
+++ precache-manifest.9641aafc307c0bef0dafb5d40a2a8913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4306ad1044517987207fb2114a1ff9b5",
+    "url": "/react-memory-game/static/media/cuccoEvolBackground.4306ad10.jpg"
+  },
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "b765571cc240bb30b066",
+    "url": "/react-memory-game/static/js/main.b765571c.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/react-memory-game/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "b765571cc240bb30b066",
+    "url": "/react-memory-game/static/css/main.4896e45c.chunk.css"
+  },
+  {
+    "revision": "dae2e42c78d9947581e96d1e1edb00e0",
+    "url": "/react-memory-game/index.html"
+  }
+];
